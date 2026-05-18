30fb68f7e40f2804d31b535180062d0006cb27c6
Arjun Bahadur Panta <kit25a.ajn@ismt.edu.np>
2026-05-18 14:24:50 +0545

    added if

--- ifStatements/script.js
+++ ifStatements/script.js
@@ -1,3 +1,12 @@
 // IF Statements
 // If a condition is true, execute some code and if not, do something else
 
+let age = 13;
+
+if (age >= 18) {
+  console.log("You'r old enough to enter this site.")
+}
+
+else {
+  console.log("You'r not old enough to enter this site.")
+}
